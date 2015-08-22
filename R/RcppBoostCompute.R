36e3c8322cdf5d065135a5827afcba3c7d591d31
Marc Suchard <msuchard@gmail.com>
2015-08-22 11:37:28 -0700

    clean R CMD check build

--- R/RcppBoostCompute.R
+++ R/RcppBoostCompute.R
@@ -6,6 +6,15 @@
 #' @import BoostCompiledLibraries
 #' @import BH
 #' @import Rcpp
-#' 
+NULL
+
+
+#' Print out OpenCL devices
+#'
+#' @name compute_hello_world
+#'
+#' @description
+#' Lists the available OpenCL devices on system.
+#'
 #' @export compute_hello_world
 NULL
